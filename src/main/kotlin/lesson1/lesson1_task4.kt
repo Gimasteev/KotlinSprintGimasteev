6f75d240db5ae5a4bac02736d3f66aac6183da82
gimasteevroman <best.roma@gmail.com>
2024-07-10 18:10:25 +0500

    1) Создал переменные и указал явно их тип 2) Распечатал в консоль

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson1
+
+fun main(){
+    val length:Long = 40_868_600_000L
+    val age:Byte = 27
+    val partOfDay:Float = 0.075f
+    val seconds:Int = 6480
+    val partOfYear:Double = 0.00020547945205479453
+    val apogee:Int = 327000
+    print("""
+        $length
+        $age
+        $partOfDay
+        $seconds
+        $partOfYear
+        $apogee
+    """.trimIndent())
+}
+
