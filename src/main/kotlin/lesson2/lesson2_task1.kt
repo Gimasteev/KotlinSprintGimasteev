f6b70880dad0cbf739a1345eace2331bedbaa6e7
gimasteevroman <best.roma@gmail.com>
2024-07-10 19:07:52 +0500

    Lesson2_task1_finish

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2
+
+fun main(){
+    val person1 = 3.0
+    val person2 = 4.0
+    val person3 = 3.0
+    val person4 = 5.0
+
+    val average_score: Double = (person1 + person2 + person3 + person4) / 4
+    println(average_score)
+
+}
