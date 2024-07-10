67ffd2af5152479f668b0b16600363db2a9b9e9a
gimasteevroman <best.roma@gmail.com>
2024-07-10 17:47:43 +0500

    1) Создал новую переменную год, час и минута запуска полета в космос 2) Распечатал в консоль 3) Переинициализировал час и минуту 4) Распечатал в консоль

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    print("Год полета – $year год\nЧас - $hour\nМинута - $minute")
+    hour = 10
+    minute = 55
+    print("\nВремя посадки\nЧас - $hour\nМинута - $minute")
+}
