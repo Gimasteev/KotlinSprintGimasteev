a5b25bd0e2579362bba4469ac1a2081f4713be21
gimasteevroman <best.roma@gmail.com>
2024-07-10 16:51:55 +0500

    1) Инициировал переменую для количества заказов 2) Текст с благодарностью за покупку

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_1
+
+// Тестовое решение к уроку 1 задаче 1
+
+fun main(){
+    var order_quantity: Int
+    val purchase_thankYou_text = "Спасибо за покупку, хорошего дня!"
+}
