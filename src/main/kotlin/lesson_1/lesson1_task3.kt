11022f2110f515140467353a09a16e4eeb4a0873
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:08:05 +0500

    Lesson1_task3_finish Внес исправления: 1) Изменил тип переменных на String 2) Добавил нули для правильного отображения 3) Отформатировал текст через Ctrl+Alt+L 4) Отформатировал вывод времени посадки согласно примеру

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_1
 
-fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
-    print("Год полета – $year год\nЧас - $hour\nМинута - $minute")
-    hour = 10
-    minute = 55
-    print("\nВремя посадки\nЧас - $hour\nМинута - $minute")
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+    print("Год полета – $year год\nЧасы - $hour\nМинуты - $minute")
+    hour = "10"
+    minute = "55"
+    print("\nВремя посадки: $hour:$minute")
 }
