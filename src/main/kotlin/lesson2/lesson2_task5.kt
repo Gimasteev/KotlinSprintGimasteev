6e492a9888a51d313078ad2127d584a40e62cbfa
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:42:06 +0500

    Lesson2_task5_finish Внес исправления: 1) Отформатировал текст через ctrl+alt+L

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -8,8 +8,8 @@ const val START_SUMM = 70_000
 const val YEAR_PERCENT = 0.167
 const val INVESTMENT_PERIOD = 20
 
-fun main(){
-    val totalAmount:Double = START_SUMM * (1 + YEAR_PERCENT).pow(INVESTMENT_PERIOD)
+fun main() {
+    val totalAmount: Double = START_SUMM * (1 + YEAR_PERCENT).pow(INVESTMENT_PERIOD)
     val df = DecimalFormat("#.###")
     df.roundingMode = RoundingMode.DOWN
     print("Размер вклада через $INVESTMENT_PERIOD лет составит = ${df.format(totalAmount)}")
