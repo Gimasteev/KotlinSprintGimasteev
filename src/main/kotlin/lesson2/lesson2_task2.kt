ab7f50dc96510a8555bbbbae13e6a862cb49d993
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:37:16 +0500

    Lesson2_task2_finish Внес исправления: 1) Исправил переменные на camel case 2) Отформатировал текст через ctrl+alt+L

--- src/main/kotlin/lesson2/lesson2_task2.kt
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -6,11 +6,11 @@ const val NUMBER_OF_INTERNS = 30
 const val NUMBER_OF_PERMANENT_EMPLOYEES = 50
 
 fun main() {
-    val expenditure_of_permanent_employees = SALARIES_OF_PERMANENT_EMPLOYEES * NUMBER_OF_PERMANENT_EMPLOYEES
-    println("Расходы на выплату зарплаты постоянных сотрудников = $expenditure_of_permanent_employees")
-    val expenditure_all = SALARIES_OF_PERMANENT_EMPLOYEES * NUMBER_OF_PERMANENT_EMPLOYEES +
+    val expenditureOfPermanentEmployees = SALARIES_OF_PERMANENT_EMPLOYEES * NUMBER_OF_PERMANENT_EMPLOYEES
+    println("Расходы на выплату зарплаты постоянных сотрудников = $expenditureOfPermanentEmployees")
+    val expenditureAll = SALARIES_OF_PERMANENT_EMPLOYEES * NUMBER_OF_PERMANENT_EMPLOYEES +
             SALARIES_OF_INTERNS * NUMBER_OF_INTERNS
-    println("Общие расходы по ЗП = $expenditure_all")
-    val average_salary_of_one_employee = expenditure_all / (NUMBER_OF_INTERNS + NUMBER_OF_PERMANENT_EMPLOYEES)
-    print("Средняя ЗП одного сотрудника = $average_salary_of_one_employee")
+    println("Общие расходы по ЗП = $expenditureAll")
+    val averageSalaryOfOneEmployee = expenditureAll / (NUMBER_OF_INTERNS + NUMBER_OF_PERMANENT_EMPLOYEES)
+    print("Средняя ЗП одного сотрудника = $averageSalaryOfOneEmployee")
 }
