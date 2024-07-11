0eb1d8549a003cc5b25b2603f685d1f4e3c0f3a3
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:35:35 +0500

    Lesson2_task1_finish Внес исправления: 1) Исправил переменную на camel case 2) Отформатировал текст через ctrl+alt+L 3) Избавился от магических чисел

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -2,9 +2,10 @@ package org.example.lesson2
 
 const val TIME_START = 579 // указал время старта в минутах
 const val TIME_TRAVELLING = 457
+const val MINUTES_IN_HOUR = 60
 
-fun main(){
-    val time_end_hours = (TIME_START + TIME_TRAVELLING) / 60
-    val time_end_minutes = (TIME_START + TIME_TRAVELLING) % 60
-    print("Время прибытия поезда = $time_end_hours:$time_end_minutes")
+fun main() {
+    val timeEndHours = (TIME_START + TIME_TRAVELLING) / MINUTES_IN_HOUR
+    val timeEndMinutes = (TIME_START + TIME_TRAVELLING) % MINUTES_IN_HOUR
+    print("Время прибытия поезда = $timeEndHours:$timeEndMinutes")
 }
