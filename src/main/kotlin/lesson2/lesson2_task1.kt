178537082124b68bc40e36bd21f2f319ba710ba0
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:31:27 +0500

    Lesson2_task1_finish Внес исправления: 1) Исправил переменную на camel case 2) Отформатировал текст через ctrl+alt+L 3) Избавился от магических чисел

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,12 +1,11 @@
 package org.example.lesson2
 
-fun main(){
-    val person1 = 3.0
-    val person2 = 4.0
-    val person3 = 3.0
-    val person4 = 5.0
-
-    val average_score: Double = (person1 + person2 + person3 + person4) / 4
-    println(average_score)
-
+const val EGLISH_LANGUAGE_SCORE_PERSON1 = 3.0
+const val EGLISH_LANGUAGE_SCORE_PERSON2 = 4.0
+const val EGLISH_LANGUAGE_SCORE_PERSON3 = 3.0
+const val EGLISH_LANGUAGE_SCORE_PERSON4 = 5.0
+fun main() {
+    val averageScore: Double =
+        (EGLISH_LANGUAGE_SCORE_PERSON1 + EGLISH_LANGUAGE_SCORE_PERSON2 + EGLISH_LANGUAGE_SCORE_PERSON3 + EGLISH_LANGUAGE_SCORE_PERSON4) / 4
+    println(averageScore)
 }
