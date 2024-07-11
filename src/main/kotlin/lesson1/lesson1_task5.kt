809c97f0146ac5c2ccbbc56a655b0e4b823177bb
gimasteevroman <best.roma@gmail.com>
2024-07-11 13:24:50 +0500

    Lesson1_task3_finish Внес исправления: 1) Создал константу для хранения всех голых чисел 2) Исправил переменную на camel case 3) Отформатировал текст через ctrl+alt+L

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,9 +1,11 @@
 package org.example.lesson1
-//Секунды – 6480
-fun main(){
-    val seconds = 6480
-    val hours = seconds / 3600
-    val minutes = (seconds % 3600) / 60
-    val last_seconds = (seconds % 60) % 60
-    println("$hours:$minutes:$last_seconds")
+
+const val SECONDS_IN_SPACE = 6480
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+fun main() {
+    val hours = SECONDS_IN_SPACE / SECONDS_IN_HOUR
+    val minutes = (SECONDS_IN_SPACE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val lastSeconds = (SECONDS_IN_SPACE % SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
+    println("$hours:$minutes:$lastSeconds")
 }
