73df502409fb7afb0bf17a5888e9eb4fe6f789fd
gimasteevroman <best.roma@gmail.com>
2024-07-12 12:22:40 +0500

    Lesson1_task5_finish 1) Ввел переменную secondsInSpace в main 2) Создал переменную formattedString и отформатировал ее согласно условию задачи

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,11 +1,12 @@
 package org.example.lesson1
 
-const val SECONDS_IN_SPACE = 6480
 const val SECONDS_IN_HOUR = 3600
 const val SECONDS_IN_MINUTE = 60
 fun main() {
-    val hours = SECONDS_IN_SPACE / SECONDS_IN_HOUR
-    val minutes = (SECONDS_IN_SPACE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
-    val lastSeconds = (SECONDS_IN_SPACE % SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
-    println("$hours:$minutes:$lastSeconds")
+    val secondsInSpace = 6480
+    val hours = secondsInSpace / SECONDS_IN_HOUR
+    val minutes = (secondsInSpace % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val lastSeconds = (secondsInSpace % SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
+    val formattedString = String.format("%02d:%02d:%02d", hours, minutes, lastSeconds)
+    print(formattedString)
 }
