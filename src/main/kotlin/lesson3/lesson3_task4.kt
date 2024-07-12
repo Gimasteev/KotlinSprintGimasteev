34e2a665edbc98c80dd5227d21a476627d3bb93f
gimasteevroman <best.roma@gmail.com>
2024-07-12 09:05:32 +0500

    Lesson3_task4_finish

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main() {
+    var fromPlace = "E2"
+    var toPlace = "E4"
+    var strokeNumber = 1
+    println("[$fromPlace-$toPlace;$strokeNumber]")
+    fromPlace = "D2"
+    toPlace = "D1"
+    strokeNumber = 2
+    print("[$fromPlace-$toPlace;$strokeNumber]")
+}
