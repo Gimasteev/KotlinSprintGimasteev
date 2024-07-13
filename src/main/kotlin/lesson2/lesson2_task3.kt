fa333d00a0d626591c8640e57354d03f522a93a3
Gimasteev <best.roma7@gmail.com>
2024-07-13 22:30:56 +0500

    lesson2_task1_corrections 1) Исправил константы 2) Переинициализировал входящие данные времени старта

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,11 +1,13 @@
 package org.example.lesson2
 
-const val TIME_START = 579 // указал время старта в минутах
-const val TIME_TRAVELLING = 457
 const val MINUTES_IN_HOUR = 60
 
 fun main() {
-    val timeEndHours = (TIME_START + TIME_TRAVELLING) / MINUTES_IN_HOUR
-    val timeEndMinutes = (TIME_START + TIME_TRAVELLING) % MINUTES_IN_HOUR
+    val timeStartHours = 9
+    var timeStartMinutes = 39
+    timeStartMinutes = timeStartHours * MINUTES_IN_HOUR + 39
+    val timeTravelling = 457
+    val timeEndHours = (timeStartMinutes + timeTravelling) / MINUTES_IN_HOUR
+    val timeEndMinutes = (timeStartMinutes + timeTravelling) % MINUTES_IN_HOUR
     print("Время прибытия поезда = $timeEndHours:$timeEndMinutes")
 }
