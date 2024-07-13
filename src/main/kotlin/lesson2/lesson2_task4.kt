098ad54b8c24291a697282dd0f1791679b7920f0
Gimasteev <best.roma7@gmail.com>
2024-07-13 22:48:48 +0500

    lesson2_task1_corrections 1) Исправил константы 2) Переинициализировал входящие проценты 3) Изменил математику счета по пропорциям

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -2,13 +2,14 @@ package org.example.lesson2
 
 import kotlin.math.roundToInt
 
-const val CRYSTALLINE_ORE = 7
-const val IRON_ORE = 11
-const val TWENTY_PERSENT = 0.2
+const val ONE_HUNDRED_PERCENT = 100
 
 fun main() {
-    val crystallineOreByBuff: Double = CRYSTALLINE_ORE * TWENTY_PERSENT
-    val ironOreByBuff: Double = IRON_ORE * TWENTY_PERSENT
+    val crystallOre = 7
+    val ironOre = 11
+    val percentBuf = 20
+    val crystallineOreByBuff: Double = crystallOre * percentBuf.toDouble() / ONE_HUNDRED_PERCENT
+    val ironOreByBuff: Double = ironOre * percentBuf.toDouble() / ONE_HUNDRED_PERCENT
     print(
         "Бафф дает ${crystallineOreByBuff.roundToInt()} кристаллической руды и " +
                 "${ironOreByBuff.roundToInt()} железной руды"
