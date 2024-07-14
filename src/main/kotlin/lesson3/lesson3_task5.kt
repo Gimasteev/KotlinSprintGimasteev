1deba8e6570f9303674bd9f91aaf7c4df7d4d131
Gimasteev <best.roma7@gmail.com>
2024-07-14 17:26:07 +0500

    lesson3_task5_corrections 1) поправил код учитывая предыдущие комментарии

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson3
 
-const val STRING = "D2-D4;0"
 
 fun main() {
-    val fromPlace = STRING.substring(0, 2)
-    val toPlace = STRING.substring(3, 5)
-    val numberOfmove = STRING.substring(6, 7)
-    print("$fromPlace\n$toPlace\n$numberOfmove")
+    val string = "D2-D4;0"
+    val fromPlace = string.substring(0, 2)
+    val toPlace = string.substring(3, 5)
+    val numberOfMove = string.substring(6, 7)
+    print("$fromPlace\n$toPlace\n$numberOfMove")
 }
