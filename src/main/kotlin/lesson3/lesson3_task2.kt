2bef872728decda777b693ea5843105add5f67c9
Gimasteev <best.roma7@gmail.com>
2024-07-14 16:51:06 +0500

    lesson3_task2_corrections 1) Удалил лишние переменные и исправил код

--- src/main/kotlin/lesson3/lesson3_task2.kt
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -1,12 +1,12 @@
 package org.example.lesson3
 
 fun main() {
-    val ageBefore = 20
-    val ageAfter = 22
-    val surnameBefore = "Андреева"
-    val surnameAfter = "Сидорова"
     val name = "Татьяна"
     val patronymic = "Сергеевна"
-    println("$surnameBefore $name $patronymic, $ageBefore")
-    print("$surnameAfter $name $patronymic, $ageAfter")
+    var age = 20
+    var surname = "Андреева"
+    println("$surname $name $patronymic, $age")
+    age = 22
+    surname = "Сидорова"
+    print("$surname $name $patronymic, $age")
 }
