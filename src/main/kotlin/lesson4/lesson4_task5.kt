b0967a408be44ea7524885aab5acf201415d6e97
gimasteevroman <best.roma@gmail.com>
2024-07-15 17:32:37 +0500

    lesson4_task5_corrections
    1) Добавил константы

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,5 +1,10 @@
 package org.example.lesson4
 
+const val CREW_SIZE_MIN = 55
+const val CREW_SIZE_MAX = 70
+const val BOX_SIZE = 50
+
+
 fun main() {
     println("Наличие повреждений корпуса? (Y/N)")
     val shipBody = readLine()
@@ -9,6 +14,6 @@ fun main() {
     val box = readLine()!!.toInt()
     println("Хорошая погода? (Y/N)")
     val weather = readLine()
-    val result = (shipBody == "N" && crew >= 55 && crew <= 70 && box > 50 && (weather == "Y" || weather == "N")) || (shipBody == "Y" && crew == 70 && box >= 50 && weather == "Y")
+    val result = (shipBody == "N" && crew >= CREW_SIZE_MIN && crew <= CREW_SIZE_MAX && box > CREW_SIZE_MIN && (weather == "Y" || weather == "N")) || (shipBody == "Y" && crew == CREW_SIZE_MAX && box >= BOX_SIZE && weather == "Y")
     println("Научно-исследовательский корабль может приступить к долгосрочному плаванию: $result")
 }
