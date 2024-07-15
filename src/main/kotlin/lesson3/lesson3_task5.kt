25017e1038597639381955b634754cd7cd3cb67f
gimasteevroman <best.roma@gmail.com>
2024-07-15 14:49:08 +0500

    Lesson3_task5_corrections
    1) Внес правки с использованием split()

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,8 +3,8 @@ package org.example.lesson3
 const val STRING = "D2-D4;0"
 
 fun main() {
-    val fromPlace = STRING.substring(0, 2)
-    val toPlace = STRING.substring(3, 5)
-    val numberOfmove = STRING.substring(6, 7)
-    print("$fromPlace\n$toPlace\n$numberOfmove")
+    val split = STRING.split("-", ";")
+    println(split[0])
+    println(split[1])
+    println(split[2])
 }
