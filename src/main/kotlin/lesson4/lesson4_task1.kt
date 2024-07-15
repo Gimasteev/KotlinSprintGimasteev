67cf5b47ad2a67bb96979eca7330ba83ef62ed60
gimasteevroman <best.roma@gmail.com>
2024-07-15 08:55:48 +0500

    Lesson4_task1 1) IDEA выдала warning o том, что выражения в resultToday и resultTomorrow всегда TRUE. Могу я их проигнорировать в рамках данной задачи?
    Lesson2_task4_corrections
    1) При использовании переменной в качестве константы IDEA выдавала ошибку. Перевел в обычную val переменую и все заработало.

---
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson4
+
+const val TABLES = 13
+
+fun main() {
+    val reserveTablesToday = 13
+    val reserveTablesTomorrow = 9
+    val resultToday = reserveTablesToday < TABLES
+    val resultTomorrow =  reserveTablesTomorrow < TABLES
+    println("Доступность столиков на сегодня: $resultToday\nДоступность столиков на завтра: $resultTomorrow")
+}
