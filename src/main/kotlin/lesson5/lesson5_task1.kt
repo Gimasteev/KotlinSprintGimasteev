16f8902be332fcc858729da3c6cde7d5c04b1a25
gimasteevroman <best.roma@gmail.com>
2024-07-17 08:19:46 +0500

    lesson5_task1_corrections
    1) Внес 2 переменных в константах
    2) Сложение перенес в условие

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,9 +1,12 @@
 package org.example.lesson5
 
+const val FIRST_NUM = 2
+const val SECOND_NUM = 3
+
 fun main() {
-    println("Введите решение уравнения 2 + 2")
+    println("Введите решение уравнения: $FIRST_NUM + $SECOND_NUM")
     val result = readln().toInt()
-    if (result == 4)
+    if (result == FIRST_NUM + SECOND_NUM)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен.")
