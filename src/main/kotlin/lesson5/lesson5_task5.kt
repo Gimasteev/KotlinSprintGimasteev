c5591e405a48ea7ff47d09cddebaec5e963124fb
gimasteevroman <best.roma@gmail.com>
2024-07-18 09:12:13 +0500

    lesson5_task5_corrections
    1) Использовал функцию (0..42).random()

--- src/main/kotlin/lesson5/lesson5_task5.kt
+++ src/main/kotlin/lesson5/lesson5_task5.kt
@@ -1,12 +1,10 @@
 package org.example.lesson5
 
-import kotlin.random.Random
-
 fun main() {
     val mutSetOfRandomNumbers = mutableSetOf<Int>()
-    mutSetOfRandomNumbers.add(Random.nextInt(0, 42))
-    mutSetOfRandomNumbers.add(Random.nextInt(0, 42))
-    mutSetOfRandomNumbers.add(Random.nextInt(0, 42))
+    mutSetOfRandomNumbers.add((0..42).random())
+    mutSetOfRandomNumbers.add((0..42).random())
+    mutSetOfRandomNumbers.add((0..42).random())
     val mutSetUser = mutableSetOf<Int>()
     println("Введите первое случайное число от 0 до 42")
     var stringInput = readln().toInt()
@@ -18,12 +16,11 @@ fun main() {
     stringInput = readln().toInt()
     mutSetUser.add(stringInput)
     val coincidences = mutSetOfRandomNumbers.intersect(mutSetUser)
-    val sizeOfCoincidences = coincidences.size
-    when (sizeOfCoincidences) {
+    when (coincidences.size) {
         3 -> println("Джекпот!")
         2 -> println("Вы угадали два числа и получаете крупный приз")
         1 -> println("Вы угадали одно число и получаете утишительный приз")
         0 -> println("Вы не угадали ни одного числа :(")
     }
-    println("Выйгрышные числа: $mutSetOfRandomNumbers")
+    println("Выигрышные числа: $mutSetOfRandomNumbers")
 }
