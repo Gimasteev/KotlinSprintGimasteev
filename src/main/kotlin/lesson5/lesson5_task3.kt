8671a54c3b04bcf491c57d8f1950c7113e244aee
gimasteevroman <best.roma@gmail.com>
2024-07-18 08:53:09 +0500

    lesson5_task3_corrections
    1) Внес изменения в логике работы программы.

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -4,19 +4,23 @@ const val RIGHT_NUM_1 = 5
 const val RIGHT_NUM_2 = 10
 
 fun main() {
-    println("Выберите число от 0 до 42")
-    println("Введите первое число")
+    println("Выберите число от 0 до 42. Введите первое число")
     val customerNumberFirst = readln().toInt()
-    println("Введите второе число меньше $customerNumberFirst")
-    val customerNumberSecond = readln().toInt()
-    if (customerNumberSecond > customerNumberFirst) {
-        println("Второе число меньше первого. Повторите игру вновь.")
-        return
+    if (customerNumberFirst == RIGHT_NUM_1 || customerNumberFirst == RIGHT_NUM_2) {
+        println("Вы угадали первое число! Попробуйте угадать второе! Введите второе число")
+        val customerNumberSecond = readln().toInt()
+        if ((customerNumberSecond == RIGHT_NUM_1 && customerNumberSecond != customerNumberFirst) || (customerNumberSecond == RIGHT_NUM_2 && customerNumberSecond != customerNumberFirst))
+            println("Поздравляем! Вы выиграли главный приз!")
+        else
+            println("Вы выиграли утешительный приз!")
+    } else {
+        println("Вы не угадали первое число! Попробуйте угадать второе! Введите второе число")
+        val customerNumberSecond = readln().toInt()
+        if (customerNumberSecond == RIGHT_NUM_1 || customerNumberSecond == RIGHT_NUM_2)
+            println("Вы выиграли утешительный приз!")
+        else
+            println("Неудача!")
     }
-    if ((customerNumberFirst == RIGHT_NUM_1 || customerNumberFirst == RIGHT_NUM_2) && (customerNumberSecond == RIGHT_NUM_1 || customerNumberSecond == RIGHT_NUM_2))
-        println("Поздравляем! Вы выиграли главный приз!")
-    else if ((customerNumberFirst == RIGHT_NUM_1 || customerNumberFirst == RIGHT_NUM_2) || (customerNumberSecond == RIGHT_NUM_1 || customerNumberSecond == RIGHT_NUM_2))
-        println("Вы выиграли утешительный приз!")
-    else
-        println("Неудача!")
+
+    println("Выйгрышные числа были $RIGHT_NUM_1 и $RIGHT_NUM_2")
 }
