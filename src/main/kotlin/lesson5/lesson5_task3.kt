1e34ed232d6a0a6d318261404b395085195b280f
gimasteevroman <best.roma@gmail.com>
2024-07-19 08:34:40 +0500

    lesson5_task3_corrections
    1) Убрал лишние текстовые сообщения
    2) поправил ввод чисел пользователем
    3) поправил строку

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -4,18 +4,18 @@ const val RIGHT_NUM_1 = 5
 const val RIGHT_NUM_2 = 10
 
 fun main() {
-    println("Выберите число от 0 до 42. Введите первое число")
+    println("Введите два числа от 0 до 42")
     val customerNumberFirst = readln().toInt()
+    val customerNumberSecond = readln().toInt()
+
     if (customerNumberFirst == RIGHT_NUM_1 || customerNumberFirst == RIGHT_NUM_2) {
-        println("Вы угадали первое число! Попробуйте угадать второе! Введите второе число")
-        val customerNumberSecond = readln().toInt()
-        if ((customerNumberSecond == RIGHT_NUM_1 && customerNumberSecond != customerNumberFirst) || (customerNumberSecond == RIGHT_NUM_2 && customerNumberSecond != customerNumberFirst))
+        if ((customerNumberSecond == RIGHT_NUM_1 && customerNumberSecond != customerNumberFirst) ||
+            (customerNumberSecond == RIGHT_NUM_2 && customerNumberSecond != customerNumberFirst)
+        )
             println("Поздравляем! Вы выиграли главный приз!")
         else
             println("Вы выиграли утешительный приз!")
     } else {
-        println("Вы не угадали первое число! Попробуйте угадать второе! Введите второе число")
-        val customerNumberSecond = readln().toInt()
         if (customerNumberSecond == RIGHT_NUM_1 || customerNumberSecond == RIGHT_NUM_2)
             println("Вы выиграли утешительный приз!")
         else
